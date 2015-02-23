5bfa7541c8ba0bb33507d279ffaa33417afc84e6
海下 直哉 <naoya-kaige@garage.co.jp>
2015-02-23 17:29:51 +0900

    first commit

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	private float smoothing = 5f;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 targetCamPos = target.position + offset;
+
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
